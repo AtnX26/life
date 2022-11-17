27d8e5a06360ef34aabc72b5aba168c3f2724bfe
Duohan Xu <atnxdh26@gmail.com>
2022-11-16 19:41:55 -0500

    Latest version of ASSIGNMENT_10

--- ASSIGNMENT_10/play.c
+++ ASSIGNMENT_10/play.c
@@ -3,23 +3,25 @@ char ga[4] ={'A','B','C', '\0'};
 
 void one(char ca[])
 {
-	printf("Value of &ca: %p\n", &ca);
-	printf("Value of &(ca[0]): %p\n", &(ca[0]));
-	printf("Value of &(ca[1]): %p\n", &(ca[1]));
+	printf("%p\n", &ca);
+	printf("%p\n", &(ca[0]));
+	printf("%p\n", &(ca[1]));
 }
 
 void two(char *pa)
 {
-	printf("Value of &pa: %p\n", &pa);
-	printf("Value of &(pa[0]): %p\n", &(pa[0]));
-	printf("Value of &(pa[1]): %p\n", &(pa[1]));
-	printf("Value of ++pa: %p\n", ++pa);
+	printf("%p\n", &pa);
+	printf("%p\n", &(pa[0]));
+	printf("%p\n", &(pa[1]));
+	printf("%p\n", ++pa);
 }
 
 int main()
 {
 	char ca[4];
 	char *pa;
+	one (ca);
+	two (pa);
 	one(ga);
 	two(ga);
 	printf("Value of &ga: %p\n", &ga);
